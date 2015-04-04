8c349cb57dbc459845514cd4c4e6a6a4bb2b7890
vokoshyv <ronsuij@gmail.com>
2015-04-04 10:26:24 -0700

    keep track of user's score/collision count

--- watchout.js
+++ watchout.js
@@ -4,7 +4,9 @@ var dataset = [1,2,3,4,5,6,7,8,9,10];
 var width = 700;
 var height = 450;
 var playerPointer = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z'
-
+var collisionCounter = 0;
+var currentScoreCount = 0;
+var highScoreCount = 0;
 
 var svgCircleContainer = d3.select(".playground").append("svg")
   .attr("width", 200)
@@ -105,24 +107,47 @@ function randomLocation(){
 };
 setInterval(randomLocation, 2500);
 
+
+
+
 function collisionChecker(){
 
   return enemies.each(function(d, index){
     // debugger
     if(checkSingleCollision(enemies[0][index])){
-      return true;
+      //cause collision counter to increase;
+      collisionCounter++;
+      d3.select('.collisions')
+        .text("Collisions: " + collisionCounter);
+        updateScore();
     }
-    return false;
   });
-
-  // use enemies locations
-  // compare against player location
-  // on intersections use jquery to up the collision counter
 
 }
 
 
-setInterval(collisionChecker, 500);
+
+function updateScore(){
+  if (currentScoreCount > highScoreCount){
+    highScoreCount = currentScoreCount;
+    d3.select('.high').text("High Score: " + highScoreCount);
+  }
+  currentScoreCount = 0;
+  d3.select('.current')
+    .text("Current Score: " + currentScoreCount);
+}
+
+
+setInterval(function(){
+  currentScoreCount++
+  d3.select('.current').text("Current Score: " + currentScoreCount);
+  if (currentScoreCount > highScoreCount){
+    highScoreCount = currentScoreCount;
+    d3.select('.high').text("High Score: " + highScoreCount);
+  }
+}, 50);
+setInterval(collisionChecker, 150);
+
 
 function checkSingleCollision(cir1){
 
@@ -131,53 +156,7 @@ function checkSingleCollision(cir1){
   var distance = Math.sqrt(dx * dx + dy * dy);
 
   if (distance < 40) {
-      // collision detected!
-      console.log("collision");
       return true;
   }
   return false;
 }
-
-// randomLocation();
-// update(alphabet);
-
-// // Grab a random sample of letters from the alphabet, in alphabetical order.
-// setInterval(function() {
-//   update(shuffle(alphabet)
-//       .slice(0, Math.floor(Math.random() * 26))
-//       .sort());
-// }, 1500);
-//
-//
-  // .attr("x", "60")
-  // .attr("y", "60")
-  // .attr("width", "20")
-  // .attr("height", "20");
-
-// var svg = d3.select("body")
-//                 .append("svg")
-//                 .attr("width", 50)
-//                 .attr("height", 50)
-//                 .style("border", "1px solid black")
-//                 .attr("xlink:href", "img/asteroid.png")
-//                 .attr("x", "60")
-//                 .attr("y", "60")
-//                 .attr("width", "20")
-//                 .attr("height", "20");
-
-// // var text = svg.selectAll("text")
-// //                 .data([0])
-// //                 .enter()
-// //                 .append("text")
-// //                 .text("Testing")
-// //                 .attr("x", "40")
-// //                 .attr("y", "60");
-
-// var imgs = svg.selectAll("img").data([0]);
-//                 imgs.enter()
-//                 .append("svg:img")
-//                 .attr("xlink:href", "file:///D:/d3js_projects/refresh.png")
-//                 .attr("x", "60")
-//                 .attr("y", "60")
-//                 .attr("width", "20")
-//                 .attr("height", "20");
